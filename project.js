907620664f0d8e64dd4b0b16cc8fb77ddf3485c7
plisov <ewb@unassigned-hostname>
2014-04-22 17:33:55 -0400

    Made tank move and made spikes

--- project.js
+++ project.js
@@ -4,6 +4,7 @@ title('RoboTime');
 // define variables
 var mainx = 50;
 var mainy = 326;
+var tankx = 375;
 var tired_workery = 470;
 var tired_workerx = 150;
 var bulletX = mainx;
@@ -28,6 +29,7 @@ var setup = function() {
     backgroundImage = loadImage("Background2.png");
     backgroundImage.resize(800, 600);
     tired_worker = loadImage("enemy.png");
+    spike = loadImage("spike.png");
     //var pistol = loadImage("Gun.png");
     //var assault_rifle = loadImage("Assault_Rifle.png");
     //var jet = loadImage("Jet.png");
@@ -58,35 +60,52 @@ var draw = function() {
     //image(crate_with_button, 300, 325);
     //image(health_bar, 440, -3);
     //image(ammo_box, 0, 300);
+    image(spike, 315, 480);
     image(main_character, mainx, 470);
-    image(tank, 375, 375);
+    image(tank, tankx, 375);
     image(tired_worker, tired_workerx, tired_workery);
 
     if (bulletX > tired_workerx) {
 	enemyKilled = true;
+	bulletFired = false;
+	bulletX = 0;
     }
 
     if (enemyKilled) {
-	bulletFired = false;
 	//Uncomment this line when worker BUG is fixed! 
-	bulletX = 0;
 	image(killed, tired_workerx, tired_workery);
     }
 
+	//Character Movement Start
     if (keyIsPressed && keyCode === RIGHT) {
 	mainx += 2;
 
     } else if (keyIsPressed && keyCode === LEFT) {
 	mainx -= 2;
     }
+	//Character Movement End
+
+
+
+	//Tank Movement Start
+      if (keyIsPressed && keyCode === UP) {
+	tankx += 2;
+
+    } else if (keyIsPressed && keyCode === DOWN) {
+	tankx -= 2;
+    }
+	//Tank Movement End
+
+
 
     if (mainx > 600) {
 	mainx = 0;
     }
-
+	//Bullet Starts Here
     if (bulletFired) {
 	fill(166, 255, 0);
 	ellipse(bulletX, bulletY, 4, 4);
 	bulletX = bulletX + 5;
     }
+	//Bullet Ends Here
 };
